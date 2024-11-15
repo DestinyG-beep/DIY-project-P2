05dddee5f845ce3d99f065ab8196c6c840662c96
mburugudestinygakenga <mburugugakenga@gmail.com>
2024-11-15 12:17:15 +0300

    sorting issues with the To Do List

--- project/src/App.js
+++ project/src/App.js
@@ -7,35 +7,44 @@ import ToDoList from './components/ToDoList';
 function App() {
 
   return (
-    <div className="App">
-      {/* Header Section */}
-      <header className='app-header'>
-      <h1 id="h1">Do It Yourself Projects</h1>
-        <NavBar/>
-      </header>
-
-      {/* Search Form */}
+    <section>
+      <div className="App">
+        {/* Header Section */}
+        <header className='app-header'>
+        <h1 id="h1">Do It Yourself Projects</h1>
+          <NavBar/>
+        </header>
+      </div>
       
+        
+        
+      <div>
+        {/* Search Form */}
+        
+        {/* Intro Section */}
+        <section className="intro-section">
+          <h2>Get Inspired</h2>
+          <p>Type in the materials you already have, and get suggestions of projects that that you can make.</p>
+        </section>
 
-      {/* Intro Section */}
-      <section className="intro-section">
-        <h2>Get Inspired</h2>
-        <p>Type in the materials you already have, and get suggestions of projects that that you can make.</p>
-      </section>
+        {/* Featured Projects Section */}
+        <h2 id='F1'>Featured Projects </h2>
+        <ProjectCard/>
 
-      {/* Featured Projects Section */}
-      <h2 id='F1'>Featured Projects </h2>
-      <ProjectCard/>
+        {/*To Do List*/}
+        <h2>To Do List</h2>
+        <ToDoList/>
+        
 
-      {/*To Do List*/}
-      <h2>To Do List</h2>
-      <ToDoList/>
-
-      {/* Footer Section */}
-      <footer className="app-footer">
-      <p>Warning !! The content and intructions on this app were compiled only to simulate practicality. Should an accident occur from following these instructions ,the author will not be held accountable. Children should have parental or guardian supervision while doing any of the projects proposed. </p>
-      </footer>
-    </div>
+        {/* Footer Section */}
+        <footer className="app-footer">
+        <p>Warning !! The content and intructions on this app were compiled only to simulate practicality. Should an accident occur from following these instructions ,the author will not be held accountable. Children should have parental or guardian supervision while doing any of the projects proposed. </p>
+        </footer>
+      </div>
+    </section>
+    
+      
+    
   );
 }
 
--- project/src/components/ToDoList.js
+++ project/src/components/ToDoList.js
@@ -1,5 +1,23 @@
-const ToDoList = () => {
-    return (  );
-}
- 
+import React from 'react'; // Import React
+
+const ToDoList = ({ toDoList }) => {
+  return (
+    <section className="todo-list">
+      <h3>Your To-Do List</h3>
+      <ul>
+        {toDoList.length === 0 ? (
+          <p>No projects in your To-Do list.</p>
+        ) : (
+          toDoList.map((project) => (
+            <li key={project.id}>
+              <h4>{project.title}</h4>
+              <p>{project.description}</p>
+            </li>
+          ))
+        )}
+      </ul>
+    </section>
+  );
+};
+
 export default ToDoList;
