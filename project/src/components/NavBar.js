52811cc10e514659a28967d5a5ff8c8b49dc4f36
mburugudestinygakenga <mburugugakenga@gmail.com>
2024-11-15 18:47:53 +0300

    routing pages

--- project/src/components/NavBar.js
+++ project/src/components/NavBar.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav>
-      <ul>
+    <nav className="navbar">
+      <ul className="links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/projects">Projects</Link></li>
         <li><Link to="/todo">To-Do List</Link></li>
